c31abf3fa9709677d7e4fc3af13d4eed5e94ff22
solenas-takeyoshi <alto19752@gmail.com>
2024-07-22 02:43:54 +0900

    新規作成

---
+++ src/main/java/com/example/demo/repository/exam/ExamChoiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository.exam;
+
+import com.example.demo.entity.exam.ExamChoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamChoiceRepository extends JpaRepository<ExamChoice, Long> {
+}
