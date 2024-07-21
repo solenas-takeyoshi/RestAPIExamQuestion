b8cc1f6288cc18db7dba87b91d77286b37512f30
solenas-takeyoshi <alto19752@gmail.com>
2024-07-22 02:43:56 +0900

    新規作成

---
+++ src/test/java/com/examle/demo/RestApiHostSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiHostSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
